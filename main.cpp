b07b2833b760c10346c53d825cf41e536b112d80
Gres <gres@gres.biz>
2017-04-09 18:49:10 +0300

    Forbid multiple application instances.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "controller.h"
 
 #include <QApplication>
+#include <QDir>
+#include <QLockFile>
+#include <QDebug>
 
 int main (int argc, char *argv[])
 {
@@ -8,6 +11,13 @@ int main (int argc, char *argv[])
   a.setOrganizationName ("Gres");
   a.setApplicationName ("MultiDir");
   a.setQuitOnLastWindowClosed (false);
+
+  QLockFile f (QDir::home ().absoluteFilePath (".multidir.lock"));
+  if (!f.tryLock ())
+  {
+    qDebug () << "Another instance is running. Lock file is busy.";
+    return 0;
+  }
 
   Controller control;
   return a.exec ();
