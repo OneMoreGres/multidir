cb89dcb13ea2129ed9c2f12685f62502656acae7
Gres <gres@gres.biz>
2017-04-18 23:02:23 +0300

    Handle drops similar way as copy-paste.

--- filesystemmodel.cpp
+++ filesystemmodel.cpp
@@ -1,4 +1,5 @@
 #include "filesystemmodel.h"
+#include "copypaste.h"
 
 #include <QMimeData>
 #include <QUrl>
@@ -22,46 +23,5 @@ bool FileSystemModel::dropMimeData (const QMimeData *data, Qt::DropAction action
     return false;
   }
 
-  auto success = true;
-  auto to = filePath (parent) + QDir::separator ();
-
-  auto urls = data->urls ();
-  auto it = urls.constBegin ();
-
-  switch (action)
-  {
-    case Qt::CopyAction:
-      for (; it != urls.constEnd (); ++it)
-      {
-        auto path = (*it).toLocalFile ();
-        auto target = QFileInfo (path).fileName ();
-        while (QFile::exists (to + target))
-        {
-          target = tr ("copy_") + target;
-        }
-        success = QFile::copy (path, to + target) && success;
-      }
-      break;
-
-    case Qt::LinkAction:
-      for (; it != urls.constEnd (); ++it)
-      {
-        auto path = (*it).toLocalFile ();
-        success = QFile::link (path, to + QFileInfo (path).fileName ()) && success;
-      }
-      break;
-
-    case Qt::MoveAction:
-      for (; it != urls.constEnd (); ++it)
-      {
-        auto path = (*it).toLocalFile ();
-        success = QFile::rename (path, to + QFileInfo (path).fileName ()) && success;
-      }
-      break;
-
-    default:
-      return false;
-  }
-
-  return success;
+  return CopyPaste ().paste (data->urls (), fileInfo (parent), action);
 }
