f8ae4016c5e9ab17ab013377cfa0fa13cac5d8a3
Gres <gres@gres.biz>
2017-05-21 13:07:28 +0300

    Old gcc support.

--- backport.h
+++ backport.h
@@ -9,8 +9,7 @@ namespace nonstd
 template<class T, class Compare>
 constexpr const T &clamp (const T &v, const T &lo, const T &hi, Compare comp)
 {
-  return assert (!comp (hi, lo) ),
-         comp (v, lo) ? lo : comp (hi, v) ? hi : v;
+  return comp (v, lo) ? lo : comp (hi, v) ? hi : v;
 }
 
 template<class T>
