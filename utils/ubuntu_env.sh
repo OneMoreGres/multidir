22529f322174c2f5af153aeea62a3af6a6284806
Gres <gres@gres.biz>
2017-05-21 13:06:14 +0300

    Updated build script.

--- utils/ubuntu_env.sh
+++ utils/ubuntu_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#QT_BIN=/opt/qt/5.8/gcc_64/bin
-QT_BIN=$HOME/Qt/Online/5.8/gcc_64/bin
+if [ -z "$QT_BIN" ]; then
+	#QT_BIN=/opt/qt/5.8/gcc_64/bin
+	QT_BIN=$HOME/Qt/Online/5.8/gcc_64/bin
+fi
 export PATH="$QT_BIN:$PATH"
 
