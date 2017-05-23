d4b5f24e170d36a78771d05f5b233fd0f8ddbcc9
Gres <gres@gres.biz>
2017-05-23 21:13:50 +0300

    Changed appimage creator tool name.

--- utils/make_appimage.sh
+++ utils/make_appimage.sh
@@ -6,13 +6,14 @@ SELF_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SELF_PATH"/make_ubuntu.sh
 
 make INSTALL_ROOT=appdir install
-MAKER="linuxdeployqt-continuous-x86_64.AppImage"
-if [ ! -f "$MAKER" ]; then
-  wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/$MAKER"
+MAKER="linuxdeployqt"
+if [ -z `which $MAKER` ]; then
+  wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" -O $MAKER
+  export PATH="$PWD:$PATH"
   chmod a+x "$MAKER"
 fi
-"./$MAKER" ./appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+$MAKER ./appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
 unset QTDIR
 unset QT_PLUGIN_PATH
 unset LD_LIBRARY_PATH
-"./$MAKER" ./appdir/usr/share/applications/*.desktop -appimage
+$MAKER ./appdir/usr/share/applications/*.desktop -appimage
