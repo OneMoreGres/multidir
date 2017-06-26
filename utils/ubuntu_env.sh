62e00f987be8acfc3a2fb285fc9dcaad79be51c7
Gres <gres@gres.biz>
2017-06-26 21:05:46 +0300

    Changed env script.

--- utils/ubuntu_env.sh
+++ utils/ubuntu_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z "$QT_BIN" ]; then
-	#QT_BIN=/opt/qt/5.8/gcc_64/bin
-	QT_BIN=$HOME/Qt/Online/5.8/gcc_64/bin
+  #QT_BIN=/opt/qt/5.9/gcc_64/bin
+	QT_BIN=$HOME/Qt/Online/5.9/gcc_64/bin
 fi
 export PATH="$QT_BIN:$PATH"
 
