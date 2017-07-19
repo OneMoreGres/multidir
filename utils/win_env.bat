4640abc038fffed0baebb81eab57bf6e98c3fb41
Gres <gres@gres.biz>
2017-07-19 22:51:37 +0300

    Explicitly set x64 versions.

--- utils/win_env.bat
+++ utils/win_env.bat
@@ -1,6 +1,6 @@
 @echo off
 
 
-call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 set PATH=c:\Qt\5.9\msvc2015_64\bin\;c:\Program Files (x86)\NSIS\;%PATH%
 
