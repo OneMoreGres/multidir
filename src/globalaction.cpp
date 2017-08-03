47d5462a8289cc824d78f5889fec8afc71f0d955
Gres <gres@gres.biz>
2017-08-03 18:15:46 +0300

    Added global action support.

--- src/globalaction.cpp
+++ src/globalaction.cpp
@@ -82,7 +82,7 @@ void GlobalAction::triggerHotKey (quint32 nativeKey, quint32 nativeMods)
 }
 
 
-#if defined(Q_OS_LINUX)
+#ifdef Q_OS_LINUX
 #  include <QX11Info>
 #  include <X11/Xlib.h>
 #  include <xcb/xcb_event.h>
@@ -183,9 +183,10 @@ quint32 GlobalAction::nativeModifiers (Qt::KeyboardModifiers modifiers)
   return native;
 }
 
+#endif
 
 
-#elif defined(Q_OS_WIN)
+#ifdef Q_OS_WIN
 #  include <qt_windows.h>
 
 
@@ -400,4 +401,187 @@ quint32 GlobalAction::nativeModifiers (Qt::KeyboardModifiers modifiers)
   //if (modifiers & Qt::GroupSwitchModifier)
   return native;
 }
+
+#endif
+
+
+#ifdef Q_OS_MAC
+#  include <Carbon/Carbon.h>
+
+static bool isInited = false;
+static QHash<QPair<quint32, quint32>, EventHotKeyRef> hotkeyRefs;
+
+struct ActionAdapter
+{
+  static OSStatus macHandler (EventHandlerCallRef /*nextHandler*/, EventRef event,
+                              void */*userData*/)
+  {
+    EventHotKeyID id;
+    GetEventParameter (event, kEventParamDirectObject, typeEventHotKeyID, NULL,
+                       sizeof(id), NULL, &id);
+    GlobalAction::triggerHotKey (quint32 (id.signature), quint32 (id.id));
+    return noErr;
+  }
+};
+
+
+bool GlobalAction::registerHotKey (quint32 nativeKey, quint32 nativeMods)
+{
+  if (!isInited)
+  {
+    EventTypeSpec spec;
+    spec.eventClass = kEventClassKeyboard;
+    spec.eventKind = kEventHotKeyPressed;
+    InstallApplicationEventHandler (&ActionAdapter::macHandler, 1, &spec, NULL, NULL);
+    isInited = true;
+  }
+
+  EventHotKeyID id;
+  id.signature = nativeKey;
+  id.id = nativeMods;
+
+  EventHotKeyRef ref = NULL;
+  OSStatus status = RegisterEventHotKey (nativeKey, nativeMods, id,
+                                         GetApplicationEventTarget (), 0, &ref);
+  if (status != noErr)
+  {
+    LERROR () << "RegisterEventHotKey error:" << LARG (status);
+    return false;
+  }
+  else
+  {
+    hotkeyRefs.insert (qMakePair (nativeKey, nativeMods), ref);
+    return true;
+  }
+}
+
+bool GlobalAction::unregisterHotKey (quint32 nativeKey, quint32 nativeMods)
+{
+  EventHotKeyRef ref = hotkeyRefs.value (qMakePair (nativeKey, nativeMods));
+  Q_ASSERT (ref);
+  OSStatus status = UnregisterEventHotKey (ref);
+  if (status != noErr)
+  {
+    LERROR () << "UnregisterEventHotKey error:" << LARG (status);
+    return false;
+  }
+  else
+  {
+    hotkeyRefs.remove (qMakePair (nativeKey, nativeMods));
+    return true;
+  }
+}
+
+bool GlobalAction::nativeEventFilter (const QByteArray & /*eventType*/,
+                                      void */*message*/, long */*result*/)
+{
+  return false;
+}
+
+quint32 GlobalAction::nativeKeycode (Qt::Key key)
+{
+  switch (key)
+  {
+    case Qt::Key_A: return kVK_ANSI_A;
+    case Qt::Key_B: return kVK_ANSI_B;
+    case Qt::Key_C: return kVK_ANSI_C;
+    case Qt::Key_D: return kVK_ANSI_D;
+    case Qt::Key_E: return kVK_ANSI_E;
+    case Qt::Key_F: return kVK_ANSI_F;
+    case Qt::Key_G: return kVK_ANSI_G;
+    case Qt::Key_H: return kVK_ANSI_H;
+    case Qt::Key_I: return kVK_ANSI_I;
+    case Qt::Key_J: return kVK_ANSI_J;
+    case Qt::Key_K: return kVK_ANSI_K;
+    case Qt::Key_L: return kVK_ANSI_L;
+    case Qt::Key_M: return kVK_ANSI_M;
+    case Qt::Key_N: return kVK_ANSI_N;
+    case Qt::Key_O: return kVK_ANSI_O;
+    case Qt::Key_P: return kVK_ANSI_P;
+    case Qt::Key_Q: return kVK_ANSI_Q;
+    case Qt::Key_R: return kVK_ANSI_R;
+    case Qt::Key_S: return kVK_ANSI_S;
+    case Qt::Key_T: return kVK_ANSI_T;
+    case Qt::Key_U: return kVK_ANSI_U;
+    case Qt::Key_V: return kVK_ANSI_V;
+    case Qt::Key_W: return kVK_ANSI_W;
+    case Qt::Key_X: return kVK_ANSI_X;
+    case Qt::Key_Y: return kVK_ANSI_Y;
+    case Qt::Key_Z: return kVK_ANSI_Z;
+    case Qt::Key_0: return kVK_ANSI_0;
+    case Qt::Key_1: return kVK_ANSI_1;
+    case Qt::Key_2: return kVK_ANSI_2;
+    case Qt::Key_3: return kVK_ANSI_3;
+    case Qt::Key_4: return kVK_ANSI_4;
+    case Qt::Key_5: return kVK_ANSI_5;
+    case Qt::Key_6: return kVK_ANSI_6;
+    case Qt::Key_7: return kVK_ANSI_7;
+    case Qt::Key_8: return kVK_ANSI_8;
+    case Qt::Key_9: return kVK_ANSI_9;
+    case Qt::Key_F1: return kVK_F1;
+    case Qt::Key_F2: return kVK_F2;
+    case Qt::Key_F3: return kVK_F3;
+    case Qt::Key_F4: return kVK_F4;
+    case Qt::Key_F5: return kVK_F5;
+    case Qt::Key_F6: return kVK_F6;
+    case Qt::Key_F7: return kVK_F7;
+    case Qt::Key_F8: return kVK_F8;
+    case Qt::Key_F9: return kVK_F9;
+    case Qt::Key_F10: return kVK_F10;
+    case Qt::Key_F11: return kVK_F11;
+    case Qt::Key_F12: return kVK_F12;
+    case Qt::Key_F13: return kVK_F13;
+    case Qt::Key_F14: return kVK_F14;
+    case Qt::Key_F15: return kVK_F15;
+    case Qt::Key_F16: return kVK_F16;
+    case Qt::Key_F17: return kVK_F17;
+    case Qt::Key_F18: return kVK_F18;
+    case Qt::Key_F19: return kVK_F19;
+    case Qt::Key_F20: return kVK_F10;
+    case Qt::Key_Return: return kVK_Return;
+    case Qt::Key_Enter: return kVK_ANSI_KeypadEnter;
+    case Qt::Key_Tab: return kVK_Tab;
+    case Qt::Key_Space: return kVK_Space;
+    case Qt::Key_Backspace: return kVK_Delete;
+    case Qt::Key_Escape: return kVK_Escape;
+    case Qt::Key_CapsLock: return kVK_CapsLock;
+    case Qt::Key_Option: return kVK_Option;
+    case Qt::Key_VolumeUp: return kVK_VolumeUp;
+    case Qt::Key_VolumeDown: return kVK_VolumeDown;
+    case Qt::Key_Help: return kVK_Help;
+    case Qt::Key_Home: return kVK_Home;
+    case Qt::Key_PageUp: return kVK_PageUp;
+    case Qt::Key_Delete: return kVK_ForwardDelete;
+    case Qt::Key_End: return kVK_End;
+    case Qt::Key_PageDown: return kVK_PageDown;
+    case Qt::Key_Left: return kVK_LeftArrow;
+    case Qt::Key_Right: return kVK_RightArrow;
+    case Qt::Key_Down: return kVK_DownArrow;
+    case Qt::Key_Up: return kVK_UpArrow;
+    default: return 0;
+  }
+}
+
+quint32 GlobalAction::nativeModifiers (Qt::KeyboardModifiers modifiers)
+{
+  quint32 native = 0;
+  if (modifiers & Qt::ShiftModifier)
+  {
+    native |= shiftKey;
+  }
+  if (modifiers & Qt::ControlModifier)
+  {
+    native |= cmdKey;
+  }
+  if (modifiers & Qt::AltModifier)
+  {
+    native |= optionKey;
+  }
+  if (modifiers & Qt::MetaModifier)
+  {
+    native |= controlKey;
+  }
+  return native;
+}
+
 #endif
