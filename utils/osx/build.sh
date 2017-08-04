43172c4e4bd3db0060fd94f6f3f90c4a0e301749
Gres <gres@gres.biz>
2017-08-04 10:23:37 +0300

    Added osx build scripts.

---
+++ utils/osx/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+SELF_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+# setup
+if [ ! -z "$QT_BIN" ]; then export PATH="$QT_BIN:$PATH";
+elif [ -d "/usr/local/opt/qt/bin" ]; then export PATH="/usr/local/opt/qt/bin:$PATH";
+elif [ -d "/opt/qt/bin" ]; then export PATH="/opt/qt/bin:$PATH";
+fi
+ROOT="$SELF_PATH/../.."
+VERSION=`cat "$ROOT/version"`
+
+
+# build
+lrelease "$ROOT/multidir.pro"
+qmake "$ROOT/"
+make -j2
+
+
+# test
+mkdir -p tests
+cd tests
+qmake "$ROOT/tests/"
+make -j2
+cd ..
+./tests/tests.app/Contents/MacOS/tests
+
+
+# pack
+macdeployqt multidir.app -dmg
+mv multidir.dmg multidir-$VERSION.dmg
+
+
+echo "Finished"
---
+++ utils/osx/prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+brew update
+brew install qt
