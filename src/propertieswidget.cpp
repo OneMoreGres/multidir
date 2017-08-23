214a543830dc500adbb158bf0974cc2412ba0151
Gres <gres@gres.biz>
2017-08-23 10:49:51 +0300

    Extended info in properties widget.

--- src/propertieswidget.cpp
+++ src/propertieswidget.cpp
@@ -12,19 +12,52 @@
 namespace
 {
 
-qint64 getSize (const QFileInfo &info)
+struct AggregateInfo
 {
-  qint64 result = 0;
+  qint32 files{0};
+  qint32 hidden{0};
+  qint32 links{0};
+  qint32 dirs{0};
+  qint64 size{0};
+
+  AggregateInfo &operator+= (const AggregateInfo &r)
+  {
+    files += r.files;
+    hidden += r.hidden;
+    links += r.links;
+    dirs += r.dirs;
+    size += r.size;
+    return *this;
+  }
+};
+
+AggregateInfo getAggregate (const QFileInfo &info, bool isTopLevel)
+{
+  AggregateInfo result;
   if (info.isDir ())
   {
+    if (!isTopLevel)
+    {
+      ++result.dirs;
+    }
+
     for (const auto &i: utils::dirEntries (info))
     {
-      result += getSize (i);
+      result += getAggregate (i, false);
     }
   }
   else
   {
-    result += info.size ();
+    ++result.files;
+    if (info.isHidden ())
+    {
+      ++result.hidden;
+    }
+    if (info.isSymLink ())
+    {
+      ++result.links;
+    }
+    result.size += info.size ();
   }
   return result;
 }
@@ -39,7 +72,12 @@ PropertiesWidget::PropertiesWidget (const QFileInfo &info, QWidget *parent) :
 
   layout->addRow (tr ("Name: "), new QLabel (info.fileName ()));
   layout->addRow (tr ("Path: "), new QLabel (info.absolutePath ()));
-  layout->addRow (tr ("Size: "), new QLabel (utils::sizeString (getSize (info), 3)));
+  const auto aggregate = getAggregate (info, true);
+  layout->addRow (tr ("Files: "), new QLabel (QString::number (aggregate.files)));
+  layout->addRow (tr ("Hidden: "), new QLabel (QString::number (aggregate.hidden)));
+  layout->addRow (tr ("Links: "), new QLabel (QString::number (aggregate.links)));
+  layout->addRow (tr ("Directories: "), new QLabel (QString::number (aggregate.dirs)));
+  layout->addRow (tr ("Size: "), new QLabel (utils::sizeString (aggregate.size, 3)));
   layout->addRow (tr ("Created: "), new QLabel (info.created ().toString ()));
   layout->addRow (tr ("Last modified: "), new QLabel (info.lastModified ().toString ()));
   layout->addRow (tr ("Owner: "), new QLabel (info.owner ()));
